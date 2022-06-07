1c9ffb2ef0be8e434baaebbbb8f3ef1a9b78a5fa
Mahe Karim <mahekarim@sizramsolutions.com>
2022-06-07 12:32:53 +0600

    nGHooks Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'first_project';
+
+  intervalSub; 
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello From Onit');
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
