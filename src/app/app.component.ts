b56914acdbe062f7182abc0c9f7faaca9b78b109
Mahe Karim <mahekarim@sizramsolutions.com>
2022-06-07 12:39:21 +0600

    With Error Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,14 @@ import { Component , OnDestroy, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent {
   title = 'first_project';
 
-  intervalSub; 
-
-  ngOnInit(): void {
-    this.intervalSub = setInterval(() => {
-      console.log('Hello From Onit');
-    }, 1000);
-  }
-
-  ngOnDestroy(): void {
-    if(this.intervalSub) {
-      clearInterval(this.intervalSub);
+  getMin(a, b) {
+    if(a < b) {
+      return a;
     }
+    return b;
   }
+
 }
